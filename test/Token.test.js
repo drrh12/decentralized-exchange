464b89fcddf9e4d7d69ed316d06b4a320e226e88
drrh12 <flavioandrade0201@gmail.com>
2022-01-16 08:35:31 +0000

    First test - match the token name, passed

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,14 @@
 const Token = artifacts.require("./Token");
+require("chai").use(require("chai-as-promised")).should();
 
 contract("Token", (accounts) => {
   describe("deployment", (deployment) => {
-    it("tracks the name", () => {
+    it("tracks the name", async () => {
       //Read token name here
       //The token name is flavio
+      const token = await Token.new();
+      const result = await token.name();
+      result.should.equal("flavio");
     });
   });
 });
