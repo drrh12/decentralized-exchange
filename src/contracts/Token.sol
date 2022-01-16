1748032e184a135a23d913555ef5502b97009ff6
drrh12 <flavioandrade0201@gmail.com>
2022-01-16 08:16:50 +0000

    test file created

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,4 +4,3 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Token {
     string public name = "flavio";
 }
-
---
+++ test/Token.test.js
@@ -0,0 +1,10 @@
+const Token = artifacts.require("./Token");
+
+contract("Token", (accounts) => {
+  describe("deployment", (deployment) => {
+    it("tracks the name", () => {
+      //Read token name here
+      //The token name is flavio
+    });
+  });
+});
