e0c5304ba8b89b85638231201c867fbf6f08ba14
drrh12 <flavioandrade0201@gmail.com>
2022-02-04 14:30:38 +0700

    To do list for the exchange contract

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,2 +1,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+// Deposit and Withdraw
+
+// Manage Orders - Make or Cancel
+
+// Handle Trades - Charge fees
+
+// TO DO:
+// [] Deposit Ether
+// [] Withdraw Ether
+// [] Deposit Tokens
+// [] Withdraw Tokens
+// [] Check balances
+// [] Make order
+// [] Cancel order
+// [] Fill order
+// [] Charge fees
