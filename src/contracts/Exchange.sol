6b856f1fe73f4dcc8a452f70e59c12e32c60a9ac
drrh12 <flavioandrade0201@gmail.com>
2022-02-06 17:53:49 +0700

    depositing tokens in the exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,24 +1,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Exchange {
-    //vars
-    address public feeAccount; // receive exchange fees
-    uint256 public feePercent; // the fee percentage
-
-    constructor(address _feeAccount, uint256 _feePercent) public {
-        feeAccount = _feeAccount;
-        feePercent = _feePercent;
-    }
-}
-
-// Deposit and Withdraw
-
-// Manage Orders - Make or Cancel
-
-// Handle Trades - Charge fees
+import "./Token.sol";
 
 // TO DO:
-// [] Set the fee account
+// [X] Set the fee account
 // [] Deposit Ether
 // [] Withdraw Ether
 // [] Deposit Tokens
@@ -28,3 +13,24 @@ contract Exchange {
 // [] Cancel order
 // [] Fill order
 // [] Charge fees
+
+contract Exchange {
+    //vars
+    address public feeAccount; // receive exchange fees
+    uint256 public feePercent; // the fee percentage
+
+    constructor(address _feeAccount, uint256 _feePercent) public {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+    }
+
+    function depositToken(address _token, uint _amount) public {
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+        // Which token?
+        // How much?
+        // Manage deposit - update balance
+        // Send token to this contract
+        // Emit event
+
+    }
+}
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,14 +1,17 @@
 import { tokens, EVM_REVERT } from "./helpers";
 
+const Token = artifacts.require("./Token");
 const Exchange = artifacts.require("./Exchange");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Exchange", ([deployer, feeAccount]) => {
+contract("Exchange", ([deployer, feeAccount, user1]) => {
+  let token;
   let exchange;
   const feePercent = 1;
 
   beforeEach(async () => {
+    token = await Token.new();
     exchange = await Exchange.new(feeAccount, feePercent);
   });
 
@@ -22,6 +25,18 @@ contract("Exchange", ([deployer, feeAccount]) => {
       const result = await exchange.feePercent();
       result.toString().should.equal(feePercent.toString());
     });
+  });
+
+  describe("depositing tokens", (deployment) => {
+    beforeEach(async () => {
+      await token.approve(exchange.address, tokens(10), { from:  });
+    });
+
+    decribe("success", () => {
+      it("tracks the token deposit", async () => {});
+    });
+
+    decribe("failure", () => {});
   });
 });
 3;
